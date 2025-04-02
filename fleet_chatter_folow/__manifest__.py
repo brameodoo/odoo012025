c9e8c320c5bd586f7c05450e8c260ace46fc1166
brameodoo <brame.odoo@gmail.com>
2025-04-02 12:34:56 -0600

    Update __manifest__.py

--- fleet_chatter_folow/__manifest__.py
+++ fleet_chatter_folow/__manifest__.py
@@ -1,18 +1,11 @@
-# -*- coding: utf-8 -*-
 {
     'name': 'Fleet chatter folow',
-    'version': '17.0.1.0.0',
-    'description': """ Marca campos especiales para su seguimiento en el Chatter""",
-    'summary': """ seguimiento en chatter """,
-    'author': '',
-    'website': '',
-    'category': '',
-    'depends': ['base', 'fleet'],
-    "data": [
-        "views/fleet_vehicle_inherit_view.xml",
+    'version': '1.0',
+    'depends': ['fleet'],
+    'data': [
+        'views/fleet_vehicle_inherit_view.xml',
     ],
-    'application': True,
     'installable': True,
+    'application': True,
     'auto_install': False,
-    'license': 'LGPL-3',
 }
