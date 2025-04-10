ff62c3417d904458521cb2dcb59c4fcd9ffb3981
brameodoo <brame.odoo@gmail.com>
2025-04-10 00:06:11 -0600

    Update workshop_request.py

--- fleet_car_workshop_request/models/workshop_request.py
+++ fleet_car_workshop_request/models/workshop_request.py
@@ -25,6 +25,7 @@ class WorkshopRequest(models.Model):
     is_analyst = fields.Boolean(string='Es Analista', compute='_compute_is_analyst', store=False)
     driver_id = fields.Many2one('res.partner', string='Conductor')
     description = fields.Text(string='Descripción') # Campo agregado
+    diagnosis = fields.Text(string='Diagnóstico') # Campo agregado
     request_date = fields.Date(string='Fecha de Solicitud', default=fields.Date.today, required=True) # Campo agregado
 
     @api.depends('analyst_id')
